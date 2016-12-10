000e028bd52a83b72b62ccf86efb4dd0a08eac98
Giovanni Azua <bravegag@hotmail.com>
2016-12-10 20:32:43 +0100

    Cosmetics

--- app/utils/FormatUtils.scala
+++ app/utils/FormatUtils.scala
@@ -2,16 +2,14 @@ package utils
 
 import java.text.SimpleDateFormat
 import java.util.Date
-
 import scala.util.DynamicVariable
 
 object FormatUtils {
-  val DateFormat = new DynamicVariable[SimpleDateFormat](null)
+  val DateFormat = new DynamicVariable[Option[SimpleDateFormat]](None)
   def formatTimestamp(timestamp: Long): String = {
-    val dateFormat = DateFormat.value match {
-      case null => DateFormat.value_=(new SimpleDateFormat("yyyy-dd-MM HH:mm:ss")); DateFormat.value
-      case dateFormat: SimpleDateFormat => dateFormat
+    DateFormat.value match {
+      case None => DateFormat.value_=(Some(new SimpleDateFormat("yyyy-dd-MM HH:mm:ss")))
     }
-    dateFormat.format(new Date(timestamp))
+    DateFormat.value.map(_.format(new Date(timestamp))).get
   }
 }
