325bb6b8925d5c51ca66cfde209dc161003d17b3
Giovanni Azua <bravegag@hotmail.com>
2016-12-26 15:37:36 +0100

    Cosmetics

--- app/dao/TokenActionDao.scala
+++ app/dao/TokenActionDao.scala
@@ -24,7 +24,6 @@ class TokenActionDao @Inject()(protected val dbConfigProvider: DatabaseConfigPro
 
   //------------------------------------------------------------------------
   def deleteByUser(user: UserRow, `type`: TokenActionKey.Type) : Future[Unit] = {
-    // TODO: review this implementation
     db.run(TokenAction.filter(tokenAction => tokenAction.userId === user.id && tokenAction.`type` === `type`.toString).delete.map(_ => ()))
   }
 }
