8ac65bbebceeea9602afce5aa30ffa4b103c6872
Giovanni Azua <bravegag@hotmail.com>
2016-12-26 15:35:39 +0100

    Cosmetics

--- app/actions/NoCache.scala
+++ app/actions/NoCache.scala
@@ -6,6 +6,25 @@ import play.api.mvc._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
+/**
+  * Custom Action composition implementation that disables client-side browser caching
+  * by changing the response header of the response adding multi-browser no-cache
+  * parameters. The composition can be done as follows:
+  * {{{
+  *
+  *   def link = NoCache {
+  *     deadbolt.SubjectPresent()() { implicit request =>
+  *       Future {
+  *         Ok(views.html.account.link(userService, auth))
+  *       }
+  *     }
+  *   }
+  *
+  * }}}
+  *
+  * @param action The inner action
+  * @tparam A Action type
+  */
 case class NoCache[A](action: Action[A]) extends Action[A] with HeaderNames {
   def apply(request: Request[A]): Future[Result] = {
     action(request).map { result =>
