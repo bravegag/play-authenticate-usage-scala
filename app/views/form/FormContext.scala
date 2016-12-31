bde16ace02b07ddbc17ee92caf9a25eb8653cc60
Giovanni Azua <bravegag@hotmail.com>
2016-12-31 18:30:41 +0100

    Cosmetics

--- app/views/form/FormContext.scala
+++ app/views/form/FormContext.scala
@@ -2,6 +2,16 @@ package views.form
 
 import javax.inject.{Inject, Singleton}
 
+/**
+  * Simplifies access to all available Dao implementations so that they don't have to
+  * be injected one by one and therefore, reduces the otherwise injection cluttering.
+  * @param acceptForm
+  * @param forgotPasswordForm
+  * @param loginForm
+  * @param passwordChangeForm
+  * @param passwordResetForm
+  * @param signupForm
+  */
 @Singleton
 class FormContext @Inject() (val acceptForm: AcceptForm
                            , val forgotPasswordForm: ForgotPasswordForm
