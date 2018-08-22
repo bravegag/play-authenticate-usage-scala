2d4da4d3d2b61c22de8bc047e9691421967ff4e9
Giovanni Azua <bravegag@hotmail.com>
2018-08-22 16:47:57 +0200

    Refactoring to using TrioMap and a bit more Scala-like, guarding against non initialized auth

--- app/actions/TryCookieAuthAction.scala
+++ app/actions/TryCookieAuthAction.scala
@@ -1,7 +1,6 @@
 package actions
 
 import com.feth.play.module.pa.PlayAuthenticate
-import helpers.ScalaRequestToJavaContext
 import play.api.mvc._
 import play.core.j.JavaHelpers
 import play.mvc.Http
@@ -9,13 +8,14 @@ import play.mvc.Http
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
 import collection.JavaConverters._
+import scala.collection.concurrent.TrieMap
 import scala.concurrent.duration._
 
 case class TryCookieAuthAction[A](action: Http.Context => Action[A])(implicit auth: PlayAuthenticate) extends Action[A] {
   def apply(request: Request[A]): Future[Result] = {
       val jContext = JavaHelpers.createJavaContext(request)
 
-      ScalaRequestToJavaContext.put(request.id, jContext)
+      TryCookieAuthAction.jContextDv += (request.id -> jContext)
 
       if(!auth.isLoggedIn(jContext)) {
         auth.tryAuthenticateWithCookie(jContext)
@@ -29,8 +29,6 @@ case class TryCookieAuthAction[A](action: Http.Context => Action[A])(implicit au
           secure = cookie.secure(), httpOnly = cookie.httpOnly())
       )
 
-    ScalaRequestToJavaContext.delete(request.id)
-
       scalaResult.map(_.withSession(session : _*).withCookies(cookies : _*))
     }
 
@@ -38,5 +36,7 @@ case class TryCookieAuthAction[A](action: Http.Context => Action[A])(implicit au
 }
 
 object TryCookieAuthAction {
+  lazy val jContextDv = TrieMap[Long, play.mvc.Http.Context]()
+
   def apply[A](action: Action[A])(implicit auth: PlayAuthenticate): TryCookieAuthAction[A] = TryCookieAuthAction(_ => action)
 }
--- app/security/MyDeadboltHandler.scala
+++ app/security/MyDeadboltHandler.scala
@@ -1,11 +1,10 @@
 package security
 
+import actions.TryCookieAuthAction
 import be.objectify.deadbolt.scala.models.Subject
 import be.objectify.deadbolt.scala.{AuthenticatedRequest, DeadboltHandler, DynamicResourceHandler, ExecutionContextProvider}
 import com.feth.play.module.pa.PlayAuthenticate
-import helpers.ScalaRequestToJavaContext
 import play.api.mvc._
-import play.core.j.JavaHelpers
 import services.UserService
 
 import scala.concurrent.Future
@@ -18,31 +17,43 @@ class MyDeadboltHandler()(implicit auth: PlayAuthenticate, context: ExecutionCon
 	// public
 	//------------------------------------------------------------------------
 	override def beforeAuthCheck[A](request: Request[A]): Future[Option[Result]] = Future {
-		val context = ScalaRequestToJavaContext.get(request.id)
-		if (auth.isLoggedIn(context)) {
-			// user is logged in
-			None
+    Option(auth).flatMap { auth =>
+      TryCookieAuthAction.jContextDv.get(request.id) match {
+        case Some(context) => {
+          if (auth.isLoggedIn(context)) {
+            // user is logged in
+            None
 
-		} else {
-			// user is not logged in
-			// call this if you want to redirect your visitor to the page that
-			// was requested before sending him to the login page
-			// if you don't call this, the user will get redirected to the page
-			// defined by your resolver
-			val originalUrl = auth.storeOriginalUrl(context)
-			context.flash.put("error", "You need to log in first, to view '" + originalUrl + "'")
-			// we know auth.getResolver.login is an instance of Scala's Call
-			Some(Results.Redirect(auth.getResolver.login.asInstanceOf[play.api.mvc.Call]))
-		}
-	}
+          } else {
+            // user is not logged in
+            // call this if you want to redirect your visitor to the page that
+            // was requested before sending him to the login page
+            // if you don't call this, the user will get redirected to the page
+            // defined by your resolver
+            val originalUrl = auth.storeOriginalUrl(context)
+            context.flash.put("error", "You need to log in first, to view '" + originalUrl + "'")
+            // we know auth.getResolver.login is an instance of Scala's Call
+            Some(Results.Redirect(auth.getResolver.login.asInstanceOf[play.api.mvc.Call]))
+          }
+        }
+        case _ => None
+      }
+    }.orElse(None)
+  }
 
 	//------------------------------------------------------------------------
 	override def getSubject[A](request: AuthenticatedRequest[A]): Future[Option[Subject]] = Future {
-		val context = ScalaRequestToJavaContext.get(request.id)
-    val authUser = auth.getUser(context)
-		// Caching might be a good idea here
-		val user = userService.findByAuthUser(authUser)
-		user
+    Option(auth).flatMap { auth =>
+      TryCookieAuthAction.jContextDv.get(request.id) match {
+        case Some(context) => {
+          val authUser = auth.getUser(context)
+          // Caching might be a good idea here
+          val user = userService.findByAuthUser(authUser)
+          user
+        }
+        case _ => None
+      }
+    }.orElse(None)
 	}
 
 	//------------------------------------------------------------------------
