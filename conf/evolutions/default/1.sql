2deed0b0f13871c1dc7f3ff9c78fb657d82a5c6b
Alexey Savenkov <alexey199753@gmail.com>
2018-08-29 15:16:12 +0300

    evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -73,7 +73,25 @@ CREATE TABLE user_security_permission (
 	modified TIMESTAMP,
 	PRIMARY KEY (user_id, security_permission_id),
 	FOREIGN KEY (user_id) REFERENCES "user"(id),
-	FOREIGN KEY (security_permission_id) REFERENCES security_permission(id)	
+	FOREIGN KEY (security_permission_id) REFERENCES security_permission(id)
+);
+
+CREATE TABLE user_device (
+  id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  "type" VARCHAR(50) NOT NULL,
+  fingerprint VARCHAR(500) NOT NULL,
+  created TIMESTAMP NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+CREATE TABLE gauth_recovery_token (
+  id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  token VARCHAR(60) NOT NULL,
+  created TIMESTAMP NOT NULL,
+  used TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES "user"(id)
 );
 
 CREATE OR REPLACE FUNCTION update_created()
@@ -95,6 +113,8 @@ $$ language 'plpgsql';
 CREATE TRIGGER update_modified_user BEFORE UPDATE OR INSERT ON "user" FOR EACH ROW EXECUTE PROCEDURE update_modified();
 CREATE TRIGGER update_modified_linked_account BEFORE UPDATE OR INSERT ON linked_account FOR EACH ROW EXECUTE PROCEDURE update_modified();
 CREATE TRIGGER update_created_cookie_token_series BEFORE INSERT ON cookie_token_series FOR EACH ROW EXECUTE PROCEDURE update_created();
+CREATE TRIGGER update_created_gauth_recovery_token BEFORE INSERT ON gauth_recovery_token FOR EACH ROW EXECUTE PROCEDURE update_created();
+CREATE TRIGGER update_created_user_device BEFORE INSERT ON user_device FOR EACH ROW EXECUTE PROCEDURE update_created();
 CREATE TRIGGER update_modified_cookie_token_series BEFORE UPDATE OR INSERT ON cookie_token_series FOR EACH ROW EXECUTE PROCEDURE update_modified();
 CREATE TRIGGER update_modified_user_security_role BEFORE UPDATE OR INSERT ON user_security_role FOR EACH ROW EXECUTE PROCEDURE update_modified();
 CREATE TRIGGER update_modified_token_action BEFORE UPDATE OR INSERT ON token_action FOR EACH ROW EXECUTE PROCEDURE update_modified();
@@ -117,6 +137,10 @@ DROP TABLE user_security_role CASCADE;
 DROP TABLE security_role CASCADE;
 
 DROP TABLE cookie_token_series CASCADE;
+
+DROP TABLE gauth_recovery_token CASCADE;
+
+DROP TABLE user_device CASCADE;
 
 DROP TABLE linked_account CASCADE;
 
