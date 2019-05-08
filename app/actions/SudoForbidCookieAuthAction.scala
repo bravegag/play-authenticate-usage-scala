ee557482cb59c346de75c48b0254aacf43bc3ef6
Giovanni Azua <bravegag@hotmail.com>
2019-05-08 12:05:45 +0200

    Starting to work all except NPE in SudoForbidCookieAuthAction.scala:22 if (auth.isAuthorizedWithCookie(context))

--- app/actions/SudoForbidCookieAuthAction.scala
+++ app/actions/SudoForbidCookieAuthAction.scala
@@ -7,9 +7,6 @@ import play.api.mvc._
 
 import scala.concurrent.Future
 import play.api.mvc.Results._
-import play.mvc.Http
-
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Custom action that checks whether an user is attempting to access a sudo protected area e.g. accessing payments
@@ -17,21 +14,21 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * her to the re-login view.
   * @param action The inner action
   * @param auth The PlayAuthenticate singleton
-  * @param ctx The context
+  * @param context The context
   * @tparam A the type of the request body
   */
-case class SudoForbidCookieAuthAction[A](action: Action[A])(implicit auth: PlayAuthenticate, ctx: play.mvc.Http.Context) extends Action[A] {
+case class SudoForbidCookieAuthAction[A](action: Action[A])(implicit auth: PlayAuthenticate, context: play.mvc.Http.Context) extends Action[A] {
   def apply(request: Request[A]): Future[Result] = {
-    if (auth.isAuthorizedWithCookie(ctx)) {
+    if (auth.isAuthorizedWithCookie(context)) {
       // save url in session
-      ctx.session().put(SessionKey.REDIRECT_TO_URI_KEY, request.uri)
+      context.session().put(SessionKey.REDIRECT_TO_URI_KEY, request.uri)
       Future.successful(Redirect(routes.Application.relogin()).flashing(FlashKey.FLASH_ERROR_KEY -> "Please type password again to access requested page"))
     } else {
       action(request)
     }
   }
 
-  override def executionContext = global
+  override def executionContext = action.executionContext
 
-  override val parser: BodyParser[A] = new play.mvc.BodyParser.Empty().asInstanceOf[BodyParser[A]]
+  override val parser: BodyParser[A] = action.parser
 }
